e6cf36f678f7e4b3ce180817dee8a378a5e4e68c
Marius <mariusaklages@gmail.com>
2018-11-06 22:54:25 +0100

    Initialize

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,10 @@
 import java.io.*;
 
 public class HelloWorld {
-	public void runHello () throws Exception
-	{
-		FileReader res = new FileReader("//Users//Marius//IdeaProjects//coursera//hello_unicode.txt");
-		int line;
-		while ((line = res.read()) != -1)
-			System.out.print((char) line);
-	}
+    public static void main(String[] args) throws Exception {
+        FileReader res = new FileReader("//Users//Marius//IdeaProjects//coursera//hello_unicode.txt");
+        int line;
+        while ((line = res.read()) != -1)
+            System.out.print((char) line);
+    }
 }
